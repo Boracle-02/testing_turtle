863aa0650109623f6ff3f8ca3cb69ac74ef2f619
Ynot <83676855+Boracle-02@users.noreply.github.com>
2022-12-19 23:43:47 -0800

    Update analog_clock.py

--- analog_clock.py
+++ analog_clock.py
@@ -14,28 +14,33 @@ dimension = width if width < height else height
 # idk arbitrary size i like this one the most on my computer
 window.setup(width = dimension // 3 * 2, height = dimension // 3 * 2)
 
-
-# window.bgcolor("black")
+# gray-ish
+window.bgcolor("#454441")
 window.title("Clock ig")
 
 # this means that the turtle will not make its little drawing animation
 # essentially you have to call .update to show
 # since we are doing a clock, we do not want a drawing animation, we want the little clock to move instantly!
-window.tracer(0)
+# window.tracer(0)
 # this breaks the circle weirdy??
 
 pen = turtle.Turtle()
 # pen.hideturtle()
 pen.speed(0)
 pen.pensize(3)
+pen.hideturtle()
 
 # now draw the circle for the clock
+# since dimension is the width of the entire screen, we want our radius to be less than half the screen so it shows
+# //4 is the first one that shows it all so I will use that since it is also even!
 pen.up()
-pen.goto(0, 210)
+pen.goto(0, dimension//4)
 pen.setheading(180)
-pen.color("green")
+
+# neon green
+pen.color("#00ff22")
 pen.pendown()
-pen.circle(210)
+pen.circle(dimension//4)
 
 
 window.mainloop()
